4b0b8b1d72bc59973f3a0b70303ff6af3b82299b
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-15 15:19:51 -0600

    Adding svg

--- src/components/HangedComponent/HangedComponent.js
+++ src/components/HangedComponent/HangedComponent.js
@@ -1,8 +1,14 @@
-const h1 = document.createElement('h1');
-
 const hangedComponent = (oportunities) => {
-    h1.innerText = oportunities;
-    return h1;
+    const fragment = document.createDocumentFragment();
+    for (let index = 0; index < oportunities; index++) {
+        const img = document.createElement('img');
+        img.src = './src/img/heart.svg';
+        img.alt = "life";
+
+        fragment.append(img);
+    }
+
+    return fragment;
 }
 
 export {
