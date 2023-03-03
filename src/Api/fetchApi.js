d8faa559cc786bb86591082f459000d30b63c23b
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-03-03 15:14:59 -0600

    Fixing api bug

--- src/Api/fetchApi.js
+++ src/Api/fetchApi.js
@@ -1,10 +1,10 @@
 const getRandomWordl = async () => {
     try {
-        return await fetch("https://random-words-api.vercel.app/word/")
+        return await fetch("https://random-word-api.herokuapp.com/word")
             .then((response) => {
                 return response.json();
             }).then((json) => {
-                return json[0].word.toUpperCase();
+                return json[0].toUpperCase();
             })
     } catch (error) {
         throw error
